bbd95e3279bd83db9497c1fce7f541ea610a3a4d
henmeh <143960925+henmeh@users.noreply.github.com>
2024-08-15 17:11:55 +0200

    helper.py::added function for eea and mod_inverse with testing

--- helperfunctions/helper.py
+++ helperfunctions/helper.py
@@ -1,10 +1,35 @@
 def ggT(number1, number2):
-    if number1 <= number2:
-        raise ValueError("First number must be bigger than second number")
-    elif not isinstance(number1, int) and isinstance(number2, int):
+    if not isinstance(number1, int) and isinstance(number2, int):
         raise TypeError("Only integers are allowed")
     else:
         remainder = number1 % number2
         if remainder == 0:
             return number2
         return ggT(number2, remainder)
+    
+
+def eea(number1, number2):
+    if not isinstance(number1, int) and isinstance(number2, int):
+        raise TypeError("Only integers are allowed")
+    else:
+        if number1 == 0:
+            return number2, 0, 1
+        else:
+            ggt, s1, t1 = eea(number2 % number1, number1)
+
+            s = t1 - (number2 // number1) * s1
+            t = s1
+    
+    return ggt, s, t
+
+
+def mod_inverse(number1, modulo):
+    if not isinstance(number1, int) and isinstance(modulo, int):
+        raise TypeError("Only integers are allowed")
+    else:
+        ggt, s, _ = eea(number1, modulo)
+
+        if ggt !=1:
+            raise ValueError("No modular inverse")
+        
+        return s % modulo
--- tests/test_helper.py
+++ tests/test_helper.py
@@ -1,7 +1,7 @@
 import unittest
 import math
 import random
-from helperfunctions.helper import ggT
+from helperfunctions.helper import ggT, eea, mod_inverse
 
 
 class Test(unittest.TestCase):
@@ -13,21 +13,51 @@ class Test(unittest.TestCase):
         for _ in range(100):
             a = random.randint(0, 10000000)
             b = random.randint(0, 10000000)
-            if a > b:
-                self.assertEqual(ggT(a, b), math.gcd(a, b))
-            else:
-                self.assertEqual(ggT(b, a), math.gcd(b, a))
+            self.assertEqual(ggT(a, b), math.gcd(a, b))
         
 
     def test_ggT_for_type_error(self):
         with self.assertRaises(TypeError):
             ggT(4.5, 3)
-        
+            ggT(4, 3.5)
+            ggT(4.5, 3.5)
+    
 
-    def test_ggT_for_value_error(self):
+    def test_eea_for_correct_result(self):
+        self.assertEqual(eea(4, 27), (1, 7, -1), "Should be (1, 7, -1)")
+        self.assertEqual(eea(6, 29), (1, 5, -1), "Should be (1, 5, -1)")
+        self.assertEqual(eea(8, 3), (1, -1, 3), "Should be (1, -1, 3)")
+        self.assertEqual(eea(7, 5), (1, -2, 3), "Should be (1, -2, 3)")
+        self.assertEqual(eea(3, 4), (1, -1, 1), "Should be (1, -1, 1)")
+        self.assertEqual(eea(4, 2), (2, 0, 1), "Should be (2, 0, 1)")
+
+
+    def test_eea_for_type_error(self):
+        with self.assertRaises(TypeError):
+            eea(4.5, 3)
+            eea(4, 3.5)
+            eea(4.5, 3.5)
+    
+
+    def test_mod_inverse_for_correct_result(self):
+        self.assertEqual(mod_inverse(4, 27), 7, "Should be 7")
+        self.assertEqual(mod_inverse(6, 29), 5, "Should be 5")
+        self.assertEqual(mod_inverse(8, 3), 2, "Should be 2")
+        self.assertEqual(mod_inverse(7, 5), 3, "Should be 1")
+        self.assertEqual(mod_inverse(3, 4), 3, "Should be 1")
+    
+
+    def test_mod_inverse_for_value_error(self):
         with self.assertRaises(ValueError):
-            ggT(301, 973)
+            mod_inverse(4, 2)
+    
 
+    def test_mod_inverse_for_type_error(self):
+        with self.assertRaises(TypeError):
+            mod_inverse(4.5, 3)
+            mod_inverse(4, 3.5)
+            mod_inverse(4.5, 3.5)
+        
 
 if __name__ == '__main__':
     unittest.main()
