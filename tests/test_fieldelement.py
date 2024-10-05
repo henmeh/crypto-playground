f280705977611a85eecab8029f8dbcb0f06ab131
henmeh <143960925+henmeh@users.noreply.github.com>
2024-10-05 17:24:44 +0200

    test_fieldelement.py::finished testcases

--- tests/test_fieldelement.py
+++ tests/test_fieldelement.py
@@ -38,6 +38,58 @@ class Test(unittest.TestCase):
         for test_case in test_cases:
             self.assertEqual(test_case[0], test_case[1])
 
+        with self.assertRaises(TypeError, msg="Numbers must be in the same modulo field"):
+            FieldElement(17,30) + FieldElement(21,31)
+
+
+    def test_sub(self):
+        test_cases = [(FieldElement(29,31) - FieldElement(4,31), FieldElement(25,31)),
+                      (FieldElement(15,31) - FieldElement(30,31), FieldElement(16,31))]
+        
+        for test_case in test_cases:
+            self.assertEqual(test_case[0], test_case[1])
+
+        with self.assertRaises(TypeError, msg="Numbers must be in the same modulo field"):
+            FieldElement(17,30) - FieldElement(21,31)
+    
+
+    def test_mul(self):
+        test_cases = [(FieldElement(24,31) * FieldElement(19,31), FieldElement(22,31))]
+        
+        for test_case in test_cases:
+            self.assertEqual(test_case[0], test_case[1])
+
+        with self.assertRaises(TypeError, msg="Numbers must be in the same modulo field"):
+            FieldElement(17,30) * FieldElement(21,31)
+    
+
+    def test_pow(self):
+        test_cases = [(FieldElement(17,31)**3, FieldElement(15,31)),
+                      (FieldElement(5,31)**5 * FieldElement(18,31), FieldElement(16,31))]
+        
+        for test_case in test_cases:
+            self.assertEqual(test_case[0], test_case[1])
+    
+
+    def test_div(self):
+        test_cases = [(FieldElement(3,31) / FieldElement(24,31), FieldElement(4,31)),
+                      (FieldElement(17,31)**-3, FieldElement(29,31)),
+                      (FieldElement(4,31)**-4 * FieldElement(11,31), FieldElement(13,31))]
+        
+        for test_case in test_cases:
+            self.assertEqual(test_case[0], test_case[1])
+
+        with self.assertRaises(TypeError, msg="Numbers must be in the same modulo field"):
+            FieldElement(17,30) / FieldElement(21,31)
+    
+
+    def test_rmul(self):
+        test_cases = [(27 * (FieldElement(3,31)), FieldElement(19,31))]
+                      
+        for test_case in test_cases:
+            self.assertEqual(test_case[0], test_case[1])
+ 
+
 
 if __name__ == '__main__':
     unittest.main()
